f3422275c3e5241496783f54a24034b2254ff917
foobar404 <austinthemichaud@gmail.com>
2019-11-16 10:37:53 -0800

    add cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const server = express()
 const helmet = require("helmet")
+var cors = require('cors')
 const login_route = require("./login/login.route")
 const auth_route = require("./auth/auth.route")
 
+server.use(cors())
 server.use(helmet())
 server.use(express.json())
 server.use("/login", login_route)
