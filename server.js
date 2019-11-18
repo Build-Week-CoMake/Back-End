fb3dd64fa5c3c95eb170bc797dbf3bac0b2dbf13
abarne <celius377@gmail.com>
2019-11-18 12:25:29 -0600

    C:/Program Files/Git/upvote route added to server.js

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-const express = require("express")
-const server = express()
-const helmet = require("helmet")
-var cors = require('cors')
-const login_route = require("./login/login.route")
-const auth_route = require("./auth/auth.route")
+const express = require('express');
+const server = express();
+const helmet = require('helmet');
+var cors = require('cors');
+const login_route = require('./login/login.route');
+const auth_route = require('./auth/auth.route');
+const upvote_route = require('./upvote/upvote.route');
 
-server.use(cors())
-server.use(helmet())
-server.use(express.json())
-server.use("/login", login_route)
-server.use("/auth", auth_route)
+server.use(cors());
+server.use(helmet());
+server.use(express.json());
+server.use('/login', login_route);
+server.use('/auth', auth_route);
+server.use('/upvote', upvote_route);
 
-module.exports = server
+module.exports = server;
