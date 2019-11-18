5575376d51f977a597713f50dc438dba4bcb13d8
abarne <celius377@gmail.com>
2019-11-18 12:23:03 -0600

    skeleton endpoints and helper functions added, returning null

---
+++ upvote/upvote.model.js
@@ -0,0 +1,19 @@
+const knex = require('../database/config');
+
+module.exports = {
+	add,
+	update,
+	remove
+};
+
+function add(issue) {
+	return null;
+}
+
+function update(changes, id) {
+	return null;
+}
+
+function remove(id) {
+	return null;
+}
---
+++ upvote/upvote.route.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const db = require('./upvote.model');
+
+router.get('/', (req, res) => {
+	return null;
+});
+
+router.post('/vote', (req, res) => {
+	return null;
+});
+
+router.put('/update', (req, res) => {
+	return null;
+});
+
+module.exports = router;
