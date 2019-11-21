b4b97a088a16c4a06ac755b8f92c69a6bfff9c6b
foobar404 <austinthemichaud@gmail.com>
2019-11-21 09:40:09 -0800

    add count to upvote endpoint

--- upvote/upvote.model.js
+++ upvote/upvote.model.js
@@ -14,8 +14,28 @@ function remove(userId, issueId) {
 	return knex('upvotes').where({ user_id: userId, issue_id: issueId }).del();
 }
 
-function getUserVote(userId) {
-	return knex('upvotes')
+async function getUserVote(userId) {
+	let issues = knex('upvotes')
 		.innerJoin("issues", "issues.id", "upvotes.issue_id")
 		.where({ "upvotes.user_id": userId });
+
+	let votes = await knex("upvotes")
+		.select("*")
+
+	let reduced = votes.reduce((acc, val) => {
+		let exists = acc[val.issue_id]
+		return {
+			...acc,
+			[val.issue_id]: (exists) ? acc[val.issue_id] + 1 : 1
+		}
+	}, {})
+
+	let joined = issues.map(v => {
+		return {
+			...v,
+			count: reduced[v.id] || 0
+		}
+	})
+
+	return joined
 }
